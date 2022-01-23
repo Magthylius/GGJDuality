637dd54da5dd4343cafb9951652cd92b1fd59f57
Magthylius <56510076+Magthylius@users.noreply.github.com>
2022-01-23 18:07:22 +0800

    feat: spin power

--- Duality/Assets/_Project/Scripts/Player/PlayerController.cs
+++ Duality/Assets/_Project/Scripts/Player/PlayerController.cs
@@ -13,20 +13,77 @@ namespace Duality.Player
         public Transform Yin;
         public Transform Yang;
         public CinemachineVirtualCamera VirtualCamera;
+
+        [Header("Settings")] 
+        public float pairDistance;
+
+        public float spinPower = 5f;
+        public float initialAngularDrag = 0f;
+        public float endAngularDrag = 10f;
+
+        private Transform _main;
+        private Transform _sub;
+        private Rigidbody2D _mainRB;
+        private Rigidbody2D _subRB;
         
         private PlayerMode _mode;
+
+        private void Start()
+        {
+            Vector3 halfDistance = new Vector3(pairDistance * 0.5f, 0f, 0f);
+            Yin.position = halfDistance;
+            Yang.position = -halfDistance;
+
+            ResolveMode();
+        }
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space)) ToggleMode();
+            _mainRB.AddForce(MovementInput, ForceMode2D.Force);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                _subRB.angularDrag = initialAngularDrag;
+            }
+            
+            if (Input.GetKeyUp(KeyCode.Mouse0) || Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                _subRB.angularDrag = endAngularDrag;
+            }
+            
+            _subRB.AddForce(_sub.up * MouseInput * spinPower, ForceMode2D.Force);
         }
 
         public void ToggleMode()
         {
             _mode = _mode == PlayerMode.Yin ? PlayerMode.Yang : PlayerMode.Yin;
-            VirtualCamera.Follow = _mode == PlayerMode.Yin ? Yin : Yang;
+            ResolveMode();
         }
-        
+
+        private void ResolveMode()
+        {
+            switch (_mode)
+            {
+                case PlayerMode.Yin:
+                    _main = Yin;
+                    _sub = Yang;
+                    break;
+                
+                case PlayerMode.Yang:
+                    _main = Yang;
+                    _sub = Yin;
+                    break;
+            }
+
+            _mainRB = _main.GetComponent<Rigidbody2D>();
+            _subRB = _sub.GetComponent<Rigidbody2D>();
+            _subRB.angularDrag = endAngularDrag;
+            VirtualCamera.Follow = _main;
+        }
+
+        private Vector2 MovementInput => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        private float MouseInput => Input.GetAxis("MouseRot");
         public PlayerMode CurrentMode => _mode;
     }
 }
