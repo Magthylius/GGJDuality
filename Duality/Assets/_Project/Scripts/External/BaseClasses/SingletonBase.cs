69791024e94027cc8efabf1f5d5763d92fd10cd7
Magthylius <56510076+Magthylius@users.noreply.github.com>
2022-01-23 17:12:18 +0800

    feat: persisntent singletons

--- Duality/Assets/_Project/Scripts/External/BaseClasses/SingletonBase.cs
+++ Duality/Assets/_Project/Scripts/External/BaseClasses/SingletonBase.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 namespace Magthylius
 {
-    /// <summary>Base class for soft singleton auto initialization</summary>
+    /// <summary>Base class for soft singleton auto initialization.</summary>
+    /// <remarks>Destroys the current script if other Instance is found.</remarks>
     public class SoftSingleton<T> : MonoBehaviour where T : Component
     {
         public static T Instance;
@@ -16,7 +17,8 @@ namespace Magthylius
         }
     }
 
-    /// <summary>Base class for hard singleton auto initialization</summary>
+    /// <summary>Base class for hard singleton auto initialization.</summary>
+    /// <remarks>Destroys the whole GameObject if other Instance is found.</remarks> 
     public class HardSingleton<T> : MonoBehaviour where T : Component
     {
         public static T Instance;
@@ -24,7 +26,41 @@ namespace Magthylius
         private void Awake()
         {
             if (Instance == null) Instance = this as T;
-            else Destroy(this.gameObject);
+            else Destroy(gameObject);
+        }
+    }
+    
+    /// <summary>Base class for soft singleton auto initialization.</summary>
+    /// <remarks>Destroys the current script if other Instance is found.</remarks>
+    public class SoftSingletonPersistent<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance;
+
+        private void Awake() 
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+                DontDestroyOnLoad(this);
+            }
+            else Destroy(this);
+        }
+    }
+    
+    /// <summary>Base class for hard singleton auto initialization.</summary>
+    /// <remarks>Destroys the whole GameObject if other Instance is found.</remarks> 
+    public class HardSingletonPersistent<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+                DontDestroyOnLoad(this);
+            }
+            else Destroy(gameObject);
         }
     }
 }
