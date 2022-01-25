ac51ea724a03ebfe01eb26ae4727ed905855f3de
Magthylius <56510076+Magthylius@users.noreply.github.com>
2022-01-25 18:50:49 +0800

    Revert "feat: base custom editor funcs"
    This reverts commit f2e468fd0e752e7b1b283bdad48d1b3882da5936.

---
+++ Duality/Assets/_Project/Scripts/External/Extensions/RigidbodyExtensions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Magthylius
+{
+    public static class RigidbodyEx
+    {
+        public static float SpeedSqr(this Rigidbody rb) => rb.velocity.sqrMagnitude;
+        public static float Speed(this Rigidbody rb) => rb.velocity.magnitude;
+    }
+    
+    public static class Rigidbody2DEx
+    {
+        public static float SpeedSqr(this Rigidbody2D rb) => rb.velocity.sqrMagnitude;
+        public static float Speed(this Rigidbody2D rb) => rb.velocity.magnitude;
+    }
+}
